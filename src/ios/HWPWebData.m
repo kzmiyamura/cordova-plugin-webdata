884239043451e1b4c23a14ed70f7ced36087812e
miyamura <kzmiyamura@gmail.com>
2020-10-31 18:17:05 +0900

    iphone対応

--- src/ios/HWPWebData.m
+++ src/ios/HWPWebData.m
@@ -4,14 +4,25 @@
 
 - (void)get:(CDVInvokedUrlCommand*)command
 {
-
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"WebData, %@", name];
-
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-
+    
+    NSString* myURLString = [[command arguments] objectAtIndex:0];
+    NSURL *myURL = [NSURL URLWithString:myURLString];
+    
+    NSError *error = nil;
+    NSString *myHTMLString = [NSString stringWithContentsOfURL:myURL encoding: NSUTF8StringEncoding error:&error];
+    
+    CDVPluginResult* result = nil;
+    if (error != nil)
+    {
+        NSLog(@"Error : %@", error);
+        NSString *str = [NSString stringWithFormat:@"%@%@",@"Error : %@", error];
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:str];
+    }
+    else
+    {
+        NSLog(@"HTML : %@", myHTMLString);
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:myHTMLString];
+    }
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
